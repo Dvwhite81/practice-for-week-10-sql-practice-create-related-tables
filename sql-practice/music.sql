411cd676eef519b2bd5a0dfa612824ddef55dcd9
Dan <dvwhite1981@gmail.com>
2023-08-21 16:37:00 -0400

    Complete

--- sql-practice/music.sql
+++ sql-practice/music.sql
@@ -11,9 +11,17 @@ CREATE TABLE bands (
 CREATE TABLE musicians (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name VARCHAR(100) NOT NULL,
-  last_name VARCHAR(100)
+  last_name VARCHAR(100),
+  band_id INTEGER NOT NULL,
+  FOREIGN KEY (band_id) REFERENCES bands(id)
 );
 CREATE TABLE instruments (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   type VARCHAR(100) NOT NULL
+);
+CREATE TABLE musicians_instruments (
+  musician_id INTEGER NOT NULL,
+  instrument_id INTEGER NOT NULL,
+  FOREIGN KEY (musician_id) REFERENCES musicians(id),
+  FOREIGN KEY (instrument_id) REFERENCES instruments(id)
 );
